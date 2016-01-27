fa6af06e12f9f3875e11eb1e1d1763ef2033c767
Nicholas Ewalt <Hotrian@Gmail.com>
2016-01-27 00:43:07 -0800

    Another minor fix for webplayer
    Web doesn't seem to register collision at the same moment Win32 does, as
    this line isn't needed and it runs slightly better on Win32.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -67,6 +67,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y); // Move by fall only
             if (gameObject.transform.position.y < -15f) // We fell outside of the bounds (but our collider is disabled here)
             {
                 _restartLevel();
